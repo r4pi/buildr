d697699ff93a9777824497712c888112270c0f67
Mark Sellors <r4pi@5vcc.com>
2023-03-10 17:54:19 +0000

    Adds packaging tools

---
+++ packager/dep_test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+sudo apt install \
+   g++ \
+   gcc \
+   gfortran \
+   libbz2-dev \
+   libblas-dev \
+   libc6 \
+   libcairo2 \
+   libcurl4 \
+   libglib2.0-0 \
+   libgomp1 \
+   libicu-dev \
+   libjpeg9 \
+   liblapack-dev \
+   liblzma-dev \
+   libopenblas-base \
+   libpango-1.0-0 \
+   libpangocairo-1.0-0 \
+   libpaper-utils \
+   libpcre2-dev \
+   libpng16-16 \
+   libreadline8 \
+   libtcl8.6 \
+   libtiff5 \
+   libtk8.6 \
+   libx11-6 \
+   libxt6 \
+   make \
+   pandoc \
+   ucf \
+   unzip \
+   zip \
+   zlib1g-dev
+
---
+++ packager/package-version.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [[ ! -d /tmp/output/raspbian-bullseye ]]; then
+  mkdir -p /tmp/output/raspbian-bullseye
+fi
+
+# R 3.x requires PCRE1
+pcre_lib='libpcre2-dev'
+if [[ "${R_VERSION}" =~ ^3 ]]; then
+  pcre_lib='libpcre3-dev'
+fi
+
+fpm \
+  -s dir \
+  -t deb \
+  -v ${R_VERSION} \
+  --iteration 4 \
+  -n r-r4pi \
+  --vendor "r4pi.org" \
+  --deb-priority "optional" \
+  --deb-field 'Bugs: https://github.com/r4pi/buildr/issues' \
+  --url "http://www.r-project.org/" \
+  --description "GNU R statistical computation and graphics system" \
+  --maintainer "r4pi.org https://r4pi.org" \
+  --license "GPL-2" \
+  -p /tmp/output/raspbian-bullseye/ \
+  -d g++ \
+  -d gcc \
+  -d gfortran \
+  -d libbz2-dev \
+  -d libblas-dev \
+  -d libc6 \
+  -d libcairo2 \
+  -d libcurl4 \
+  -d libglib2.0-0 \
+  -d libgomp1 \
+  -d libicu-dev \
+  -d libjpeg62-turbo \
+  -d liblapack-dev \
+  -d liblzma-dev \
+  -d libopenblas-base \
+  -d libpango-1.0-0 \
+  -d libpangocairo-1.0-0 \
+  -d libpaper-utils \
+  -d ${pcre_lib} \
+  -d libpng16-16 \
+  -d libreadline8 \
+  -d libtcl8.6 \
+  -d libtiff5 \
+  -d libtk8.6 \
+  -d libx11-6 \
+  -d libxt6 \
+  -d make \
+  -d pandoc \
+  -d ucf \
+  -d unzip \
+  -d zip \
+  -d zlib1g-dev \
+  /opt/R/${R_VERSION}
